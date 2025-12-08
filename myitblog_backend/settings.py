8f20a66df6e02dd921993c46a328e4af990c6d12
unknown <arzyadwan@gmail.com>
2025-12-08 23:43:06 +0700

    dfsdufhsdjfhsd

--- myitblog_backend/settings.py
+++ myitblog_backend/settings.py
@@ -143,7 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",           # Untuk dev lokal
     "http://127.0.0.1:5173",
-    # "https://nama-project-anda.vercel.app",  <-- UNCOMMENT DAN ISI NANTI SETELAH DEPLOY FRONTEND
+    "https://ravellnetwork.vercel.app/",  
 ]
 
 # Agar browser mengizinkan cookie/session cross-domain (opsional tapi sering dibutuhkan)
@@ -152,8 +152,8 @@ CORS_ALLOW_CREDENTIALS = True
 # CSRF Trusted Origins (Penting untuk POST request dari Vercel)
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
-    # "https://nama-project-anda.vercel.app",  <-- UNCOMMENT DAN ISI NANTI
-    # "https://nama-backend-anda.up.railway.app", <-- UNCOMMENT DAN ISI NANTI
+    "https://ravellnetwork.vercel.app/",
+    "https://fandi-beprofile-production.up.railway.app",
 ]
 
 # Konfigurasi Media Files (Gambar)
